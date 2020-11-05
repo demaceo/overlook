14194735e2116976cfd6b49edc9d9284ec202e04
Demaceo Howard <hdemaceo@gmail.com>
2020-11-04 21:40:12 -0700

    Create basic framework for main page

--- src/index.js
+++ src/index.js
@@ -8,3 +8,9 @@ import './css/base.scss';
 import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+
+ // need to import your SCSS files in the JavaScript entry file (index.js)
+ //for the styles to be applied to your HTML.
+ //The example base.scss file has already been imported
+ //in the JavaScript entry file as an example.
