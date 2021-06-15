98b30167dc04a644d11044e4c7f9311220773582
demaceo <hdemaceo@gmail.com>
2021-06-14 21:25:43 -0600

    Beautify

--- src/classes/userRepo.js
+++ src/classes/userRepo.js
@@ -1,59 +1,67 @@
-import Booking from './bookingRepo';
-import {
-  fetchData
-} from './fetchData';
+import Booking from "./bookingRepo";
+import { fetchData } from "./fetchData";
 
 export default class User {
   constructor(userData = {}) {
     this.id = userData.id || 666;
-    this.name = userData.name || 'Manager';
+    this.name = userData.name || "Manager";
   }
 
   bookMyRoom(date, roomNumber) {
     let booking = {
-      "userID": this.id,
-      "date": date,
-      "roomNumber": roomNumber
+      userID: this.id,
+      date: date,
+      roomNumber: roomNumber,
     };
-    fetchData.createBooking(booking)
+    fetchData.createBooking(booking);
   }
 
   viewMyBookings(bookingData) {
-    let myBookings = bookingData.filter(booking => {
-      return booking.userID === this.id
+    let myBookings = bookingData.filter((booking) => {
+      return booking.userID === this.id;
     });
     return myBookings.sort((a, b) => {
       return a.date < b.date ? -1 : 1;
-    })
+    });
   }
 
   viewMyTotal(bookingData, roomData) {
-    let grandTotal = this.viewMyBookings(bookingData).reduce((myGrandTotal, myBooking) => {
-        let matchedRoom = roomData.find(room => room.number === myBooking.roomNumber);
+    let grandTotal = this.viewMyBookings(bookingData)
+      .reduce((myGrandTotal, myBooking) => {
+        let matchedRoom = roomData.find(
+          (room) => room.number === myBooking.roomNumber
+        );
         if (matchedRoom !== undefined) {
           myGrandTotal += matchedRoom.costPerNight;
         }
-          return myGrandTotal;
-        }, 0).toFixed(2);
-      return Number(grandTotal)
-    }
-
-    viewUnavailableRooms(bookingData, roomData, date) {
-      return bookingData.reduce((bookedRooms, booking) => {
-        if (booking.date === date) {
-          bookedRooms.push(roomData.find(room => room.number === booking.roomNumber))
-        }
-        return bookedRooms
-      }, []);
-    }
-
-    viewAvailableRooms(bookingData, roomData, date) {
-      let unavailableRooms = this.viewUnavailableRooms(bookingData, roomData, date);
-      return roomData.filter(room => !unavailableRooms.includes(room))
-    }
-
-    viewAvailableRoomsByType(bookingData, roomData, date, roomType) {
-      let availableRooms = this.viewAvailableRooms(bookingData, roomData, date);
-      return availableRooms.filter(room => room.roomType === roomType)
-    }
+        return myGrandTotal;
+      }, 0)
+      .toFixed(2);
+    return Number(grandTotal);
   }
+
+  viewUnavailableRooms(bookingData, roomData, date) {
+    return bookingData.reduce((bookedRooms, booking) => {
+      if (booking.date === date) {
+        bookedRooms.push(
+          roomData.find((room) => room.number === booking.roomNumber)
+        );
+      }
+      return bookedRooms;
+    }, []);
+  }
+
+  viewAvailableRooms(bookingData, roomData, date) {
+    let unavailableRooms = this.viewUnavailableRooms(
+      bookingData,
+      roomData,
+      date
+    );
+    return roomData.filter((room) => !unavailableRooms.includes(room));
+  }
+
+  viewAvailableRoomsByType(bookingData, roomData, date, roomType) {
+    let availableRooms = this.viewAvailableRooms(bookingData, roomData, date);
+    return availableRooms.filter((room) => room.roomType === roomType);
+  }
+}
